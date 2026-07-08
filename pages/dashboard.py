2d31b54de6938b635f5424be7e20a0b548e46085
allek <kushal.alle@gmail.com>
2026-07-07 22:33:33 -0400

    final

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -9,18 +9,18 @@ st.header("Welcome to the :violet[_Dashboard_] 📡", text_alignment="center", d
 lcolumn, mcolumn, rcolumn = st.columns(3)
 
 with lcolumn:
-    st.image("images\aScreenshot 2026-07-06 153552.png",
+    st.image("images/aScreenshot 2026-07-06 153552.png",
              use_container_width=True)
     st.link_button("Click Here to Play! 🎮", "https://2753628.playcode.io/")
     st.write("##")
-    st.image("images\Screenshot 2026-07-06 163126.png",
+    st.image("images/Screenshot 2026-07-06 163126.png",
              use_container_width=True)
     st.link_button("Click Here to Play! 🎮", "https://2753651.playcode.io")
 with mcolumn:
-    st.image("images\Screenshot 2026-07-06 160841.png",
+    st.image("images/Screenshot 2026-07-06 160841.png",
              use_container_width=True)
     st.link_button("Click Here to Play! 🎮", "https://2753647.playcode.io")
-    st.image("images\Screenshot 2026-07-06 180938.png",
+    st.image("images/Screenshot 2026-07-06 180938.png",
              use_container_width=True)
     st.link_button("Click Here to Play! 🎮", "https://2753669.playcode.io")
     st.write("##")
@@ -34,9 +34,9 @@ with mcolumn:
     if st.button(":red[Log out]"):
         st.switch_page("stellarhome.py")
 with rcolumn:
-    st.image("images\Screenshot 2026-07-06 162543.png",
+    st.image("images/Screenshot 2026-07-06 162543.png",
              use_container_width=True)
     st.link_button("Click Here to Play! 🎮", "https://2753649.playcode.io")
-    st.image("images\Screenshot 2026-07-06 181234.png",
+    st.image("images/Screenshot 2026-07-06 181234.png",
              use_container_width=True)
     st.link_button("Click Here to Play! 🎮", "https://2753671.playcode.io")
